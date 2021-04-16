a33d51b8ce4f94acb6445747e7b4ca8d8cdb36b9
padmajeetpawar <padmajeetpawar@gmail.com>
2021-04-15 20:47:10 -0500

    added swagger documentations for rest-apis

---
+++ src/main/java/com/springboot/webservices/restfulwebservices/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.springboot.webservices.restfulwebservices;
+
+import io.swagger.annotations.SwaggerDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+}
